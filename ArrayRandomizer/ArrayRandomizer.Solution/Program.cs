6ff4296fdb450b4c4b570d7720ac2691e36ce99d
Limey Bastow <limeybastow@Limeys-MacBook-Air.local>
2024-12-06 09:26:50 -0500

    Added ArrayRandomizer coding test.

---
+++ ArrayRandomizer/ArrayRandomizer.Solution/Program.cs
@@ -0,0 +1,28 @@
+﻿//  The starting array.
+int[] inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//  Randomize the array.
+var randomizedArray = RandomizeArray(inputArray);
+
+for(int i=0;i < inputArray.Length;i++){
+    Console.WriteLine($"{inputArray[i]}\t-\t{randomizedArray[i]}");
+}
+
+int[] RandomizeArray(int[] array){
+    // Write the code to randomize the order of the input array and return it.
+    var inputList = new List<int>(array);
+    var randomizedList = new List<int>();
+    // Extra Credit - Optimizatio.  Why?
+    // var randomizedList = new List<int>(inputList.Count());
+
+    var randomIndexGenerator = new Random();
+    while (inputList.Count() > 0){
+        var randomIndex = randomIndexGenerator.Next(0, (inputList.Count() -1));
+        var value = inputList[randomIndex];
+
+        randomizedList.Add(value);
+        inputList.RemoveAt(randomIndex);
+    }
+    
+    return randomizedList.ToArray();
+}
---
+++ ArrayRandomizer/ArrayRandomizer.Test/Program.cs
@@ -0,0 +1,16 @@
+﻿//  The starting array.
+int[] inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//  Randomize the array.
+var randomizedArray = RandomizeArray(inputArray);
+
+for(int i=0;i < inputArray.Length;i++){
+    Console.WriteLine($"{inputArray[i]}\t-\t{randomizedArray[i]}");
+}
+
+int[] RandomizeArray(int[] array){
+    // Write the code to randomize the order of the input array and return it.
+    var randomizedArray = array;
+
+    return randomizedArray;
+}
